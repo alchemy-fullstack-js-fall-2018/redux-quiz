2dac45e960227412fa1791842ceca88dfdacb1ee
mlamb <mlamb128@gmail.com>
2018-11-27 15:35:57 -0800

    Actions written and tested

--- src/actions/controls.js
+++ src/actions/controls.js
@@ -1,1 +1,11 @@
-export const 
+export const GREETING_CHANGE = 'GREETING_CHANGE';
+export const changeGreeting = input => ({
+  type: GREETING_CHANGE,
+  payload: input
+});
+
+export const NAME_CHANGE = 'NAME_CHANGE';
+export const changeName = input => ({
+  type: NAME_CHANGE,
+  payload: input
+});
