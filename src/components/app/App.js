be07f7bc333775ac7ce0a2e0749c5ae9ce0d53ef
Sarah Flynn <sarah.flynn@icloud.com>
2018-11-27 15:49:58 -0800

    name tag displaying to UI

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import Controls from '../../containers/controls/Controls';
+import Display from '../../containers/display/Display';
 
 export default function App() {
   return (
     <Fragment>
       <h1>Name Tag App</h1>
       <Controls />
+      <Display />
     </Fragment>
   );
 }
