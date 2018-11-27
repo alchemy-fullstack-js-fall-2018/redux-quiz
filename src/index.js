fbd1ec3627f96f1aafec88fa9367141dde07571c
mlamb <mlamb128@gmail.com>
2018-11-27 15:29:31 -0800

    Create store.js and wire into app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import store from './store';
+import { Provider } from 'react-redux';
 import App from './components/app/App';
 
 render(
-  <App />,
+  <Provider>
+    <App store={store} />
+  </Provider>,
   document.getElementById('root')
 );
