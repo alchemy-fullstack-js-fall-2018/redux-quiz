b98e90acdd672bef78ca292a0fc22b3464bc7016
Ryan Luras <ryanluras@gmail.com>
2018-11-27 15:59:49 -0800

    tests passing, travis added

---
+++ src/reducers/reducers.test.js
@@ -0,0 +1,33 @@
+import greetingReducer from './greeting';
+import nameReducer from './name';
+import {
+  UPDATE_GREETING_CHANGE,
+  UPDATE_NAME_CHANGE
+} from '../actions/controls';
+
+describe('Reducers', () => {
+  const initialState = {
+    greeting: 'Hello',
+    name: 'Joey'
+  };
+
+  it('Greeting Reducer handles the UPDATE_GREETING_CHANGE action', () => {
+    const newState = greetingReducer(initialState, {
+      type: UPDATE_GREETING_CHANGE,
+      payload: 'Yo'
+    });
+
+    expect(newState.greeting).toEqual('Yo');
+  });
+
+  it('Name Reducer handles the UPDATE_NAME_CHANGE action', () => {
+    const newState = nameReducer(initialState, {
+      type: UPDATE_NAME_CHANGE,
+      payload: 'Joe'
+    });
+
+    expect(newState.name).toEqual('Joe');
+  });
+
+
+});
