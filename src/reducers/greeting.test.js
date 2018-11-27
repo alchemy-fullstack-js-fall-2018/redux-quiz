ada6a80b6aaceafafb495e13c146e713915cce55
mlamb <mlamb128@gmail.com>
2018-11-27 15:45:32 -0800

    Greeting reducer written and tested

---
+++ src/reducers/greeting.test.js
@@ -0,0 +1,20 @@
+import reducer from './greeting';
+import { GREETING_CHANGE } from '../actions/controls';
+
+describe('greeting change reducer', () => {
+
+  const initialState = {
+    greeting: 'Hello'
+  };
+
+  it('handles GREETING_CHANGE action', () => {
+    const newState = reducer(initialState, { type: GREETING_CHANGE, payload: 'Yo!' });
+    expect(newState.greeting).toEqual('Yo!');
+  });
+
+  it('returns default if it doesnt recognize action', () => {
+    const newState = reducer(initialState, { type: 'NAME_CHANGE', payload: 'Yo!' });
+    expect(newState.greeting).toEqual('Hello');
+  });
+
+});
