ffca57ff1aa603d4bb7fc4bafca1ed784afce7c2
Claire Flanagan <claireflanagan@gmail.com>
2018-11-27 16:10:03 -0800

    greeting input working

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import DisplayContainer from '../../containers/display/DisplayContainer';
+import ControlsContainer from '../../containers/controls/ControlsContainer';
 
 export default function App() {
   return (
     <Fragment>
       <h1>App</h1>
       <DisplayContainer />
+      <ControlsContainer />
     </Fragment>
 
   );
