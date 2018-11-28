47a06258f01130c02704bb533b7b7a11d225797b
KLPainter <KLPainter@gmail.com>
2018-11-27 16:07:15 -0800

    added propTypes

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 const Controls = ({ greeting, name, updateGreeting, updateName }) => {
   return (
@@ -7,6 +8,13 @@ const Controls = ({ greeting, name, updateGreeting, updateName }) => {
       Greeting: <input value={greeting} onChange={updateGreeting} />
     </Fragment>
   );
+};
+
+Controls.propTypes = {
+  name: PropTypes.string.isRequired,
+  greeting: PropTypes.string.isRequired,
+  updateGreeting: PropTypes.func.isRequired,
+  updateName: PropTypes.func.isRequired
 };
 
 export default Controls;
